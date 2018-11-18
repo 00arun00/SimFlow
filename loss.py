1b5d4d633a2eb16d3a65a37e22015b2b8a692469
00arun00 <arunjoseph.ai@gmail.com>
2018-11-17 21:04:31 -0800

    adding softmax cross entropy

---
+++ loss.py
@@ -0,0 +1,36 @@
+import numpy as np
+class Loss(object):
+    '''
+    Abstract class representing a loss function
+    '''
+    def get_loss(self):
+        raise NotImplementedError('This is an abstract class')
+
+class SoftmaxCrossEntropyLoss(Loss):
+    '''
+    Represents the categorical softmax cross entropy loss
+    '''
+
+    def get_loss(self, scores, labels):
+        '''
+        Calculates the average categorical softmax cross entropy loss.
+        Args:
+            scores (numpy.ndarray): Unnormalized logit class scores. Shape (batch_size, num_classes)
+            labels (numpy.ndarray): True labels represented as ints (eg. 2 represents the third class). Shape (batch_size)
+        Returns:
+            loss, grad
+            loss (float): The average cross entropy between labels and the softmax normalization of scores
+            grad (numpy.ndarray): Gradient for scores with respect to the loss. Shape (batch_size, num_classes)
+        '''
+        scores_norm = scores - np.max(scores, axis=1, keepdims=True)
+        scores_norm = np.exp(scores_norm)
+        scores_norm = scores_norm / np.sum(scores_norm, axis=1, keepdims=True)
+
+        true_class_scores = scores_norm[np.arange(len(labels)), labels]
+        loss = np.mean(-np.log(true_class_scores))
+
+        one_hot = np.zeros(scores.shape)
+        one_hot[np.arange(len(labels)), labels] = 1.0
+        grad = (scores_norm - one_hot) / len(labels)
+
+return loss, grad
