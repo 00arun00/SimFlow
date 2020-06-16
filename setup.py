1fc99c4b2f8b12c7b8ed3043f5fcd6f5993a500b
00arun00 <arunjoseph.ai@gmail.com>
2020-06-16 00:06:03 -0700

    package creator

---
+++ setup.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='simflow',
+    version='0.0.1',
+    description='simflow',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    url='https://github.com/00arun00/SimFlow',
+    author="Arun Joseph | Gunjan Patil",
+    author_email="arunjoseph.eng@gmail.com"
+)
